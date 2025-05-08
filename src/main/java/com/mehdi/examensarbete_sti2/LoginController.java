6eab5291da1078325f848c3e13bd4b0a8dc66f03
mehdinoori20 <mehdi.noori1994@hotmail.com>
2025-05-08 05:34:47 +0200

    edit styling

--- src/main/java/com/mehdi/examensarbete_sti2/LoginController.java
+++ src/main/java/com/mehdi/examensarbete_sti2/LoginController.java
@@ -1,2 +1,12 @@
-package com.mehdi.examensarbete_sti2;public class LoginController {
+package com.mehdi.examensarbete_sti2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
 }
